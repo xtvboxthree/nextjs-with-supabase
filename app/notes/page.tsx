9feb914c8878994c8ae662f5489ec5ebae78ba7b
xtvboxthree <159789807+xtvboxthree@users.noreply.github.com>
2024-02-24 21:31:43 +0800

    Update page.tsx

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -5,11 +5,13 @@ export default async function Page() {
   const { data: notes } = await supabase.from('notes').select()
 
   //return <pre>{JSON.stringify(notes, null, 2)}</pre>
-  return (
-    <div>
-      {notes.map(note => (
-        <p key={note.id}>{note.title}</p>
-      ))}
-    </div>
-  )
+  if (!notes) {
+    return <p>No posts found.</p>
+  }
+
+  return notes.map((note) => (
+    <p key={note.id}>
+      <Link href={`/static/${note.id}`}>{note.title}</Link>
+    </p>
+  ))
 }
